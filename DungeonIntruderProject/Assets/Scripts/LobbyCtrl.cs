8267ca080c97aa8f52675311b41c7b064237503c
Siddig2000 <siddig.ku2000@gmail.com>
2025-09-18 16:41:14 +0700

    Add host and join code

--- DungeonIntruderProject/Assets/Scripts/LobbyCtrl.cs
+++ DungeonIntruderProject/Assets/Scripts/LobbyCtrl.cs
@@ -1,17 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using Mirror;
+using TMPro;
 using UnityEngine;
+using Utp;
 
 public class LobbyCtrl : MonoBehaviour
 {
     [SerializeField] private GameObject groupLobby;
+    [SerializeField] private TMP_InputField inputCode;
+    [SerializeField] private TextMeshProUGUI textHostCode;
+
+    private RelayNetworkManager relayNetworkManager;
+    void Start()
+    {
+        relayNetworkManager = NetworkManager.singleton.GetComponent<RelayNetworkManager>();
+    }
     public void OnClickedHost()
     {
-        Mirror.NetworkManager.singleton.StartHost();
+        // RelayManager.Instance.CreateRelay();
+        relayNetworkManager.StartRelayHost(4);
         groupLobby.SetActive(false);
     }
     public void OnClickedJoin()
     {
-        Mirror.NetworkManager.singleton.StartClient();
+        // RelayManager.Instance.JoinRelay(inputCode.text);
+        relayNetworkManager.relayJoinCode = inputCode.text;
+        relayNetworkManager.JoinRelayServer();
+    }
+    public void OnClickedGetCode()
+    {
+        // textHostCode.text = $"Code = {RelayManager.Instance.joinCode}";
+        textHostCode.text = $"Code = {relayNetworkManager.relayJoinCode}";
     }
 }
